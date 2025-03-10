c6948cc96501578de04e9336cefe931c77ee88a1
Fredrik Storm <fredrik@fldc.se>
2025-03-10 13:17:35 +0100

    Reformat.

--- src/main.cpp
+++ src/main.cpp
@@ -2,11 +2,11 @@
 
 void setup()
 {
-  Serial.begin(9600);
+    Serial.begin(9600);
 }
 
 void loop()
 {
-  Serial.println("Hello, World!");
-  delay(1000);
+    Serial.println("Hello, World!");
+    delay(1000);
 }
