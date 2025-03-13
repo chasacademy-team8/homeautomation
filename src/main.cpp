12bb5e91a9cf4f56a64665a07e0e6807727db394
Wilmer-Chas <wilmer.forsman@chasacademy.se>
2025-03-13 10:53:43 +0100

    first version

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,24 @@
 #include <Arduino.h>
 
-void setup()
-{
-    Serial.begin(9600);
+int photoDiodePin = A0; // used for reading
+int photoDiodeValue = 0; 
+
+// GND > Potentiometer > resistor > > A0 > potentiometer > 5V 
+
+void photoDiodeReadValue();
+
+void setup() {
+  Serial.begin(9600);  
 }
 
-void loop()
+void loop() {
+  photoDiodeReadValue();
+}
+
+void photoDiodeReadValue() 
+// used to read the electricity current flowing through photoDiodeReadValue
 {
-    Serial.println("Hello, World!");
-    delay(1000);
+    photoDiodeValue = analogRead(photoDiodePin);  
+  	Serial.println(photoDiodeValue);  
+  	delay(500);  
 }
