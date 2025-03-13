c29098a84c70a05a34f27d463676acb2cd639db4
Wilmer-Chas <wilmer.forsman@chasacademy.se>
2025-03-13 11:34:17 +0100

    second version adding more led Brightness controll depending on the ambient brightness detected by the photoresistor

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,18 @@
 #include <Arduino.h>
 
 int photoDiodePin = A0; // used for reading
+int ledPin = 9; // needs to be 9 as it can PWM enabled
 int photoDiodeValue = 0; 
 
 // GND > Potentiometer > resistor > > A0 > potentiometer > 5V 
+
+// 5V > Resistor > Led light > 9
 
 void photoDiodeReadValue();
 
 void setup() {
   Serial.begin(9600);  
+  pinMode(ledPin, OUTPUT);
 }
 
 void loop() {
@@ -21,4 +25,13 @@ void photoDiodeReadValue()
     photoDiodeValue = analogRead(photoDiodePin);  
   	Serial.println(photoDiodeValue);  
   	delay(500);  
+  	setLedLight(photoDiodeValue);
+}
+
+// sets the led light brightness
+void setLedLight(int photoDiodeValue)
+{
+  // formula to calculate brightness, where dark and high resistance equals low brightness
+  int brightness = map(photoDiodeValue, 0, 1023, 0, 255);
+  analogWrite(ledPin, brightness);  // Set LED brightness
 }
