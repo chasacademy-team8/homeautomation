df84f44a68d3b87a5347cc9b4053bef229a0522f
Viktorkvick <viktor.kvick@chasacademy.se>
2025-03-13 14:45:26 +0100

    Added smoke alarm, sound and light

--- src/main.cpp
+++ src/main.cpp
@@ -1,27 +1,58 @@
 #include <Arduino.h>
-#include <WiFiS3.h>
-#include <LiquidCrystal.h>
-#include "config.h"
 
-void ledControl(uint8_t ledPin, uint8_t state)
-{
-    analogWrite(ledPin, state);
-}
+const int smokePin = A0; // Analog pin for sensor
+const int ledPin = 4;    // LED to indicate smoke presence
+const int buzzerPin = 5; // Buzzer for an alarm
 
-void processLogic()
-{
-}
+int smokeThreshold = 100; // Default threshold
 
 void setup()
 {
+    pinMode(smokePin, INPUT);
+    pinMode(ledPin, OUTPUT);
+    pinMode(buzzerPin, OUTPUT);
     Serial.begin(9600);
 
-    pinMode(LED_PIN, OUTPUT);
+    Serial.println("Gas Sensor Warming Up...");
+    delay(2000); // Allow sensor to stabilize
 }
 
 void loop()
 {
-    processLogic();
+    gasAlarm();
+    gasLight();
+}
+
+void gasAlarm()
+{
+    float smokeLevel = analogRead(smokePin); // Read gas sensor value
+    Serial.print("Smoke Level: ");
+    Serial.println(smokeLevel); // Values for debugging
+
+    if (smokeLevel > smokeThreshold)
+    {
+        Serial.println(" Warning! Smoke detected!");
+        digitalWrite(buzzerPin, LOW);
+    }
+    else
+    {
+        Serial.println(" No Smoke Detected.");
+        digitalWrite(buzzerPin, HIGH);
+    }
+}
+
+void gasLight()
+{
+    float smokeLevel = analogRead(smokePin);
+
+    if (smokeLevel > smokeThreshold)
+    {
+        digitalWrite(ledPin, LOW);
+    }
+    else
+    {
+        digitalWrite(ledPin, HIGH);
+    }
 
     delay(1000);
 }
