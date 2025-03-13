4ab0bdd2730935ae25c83a435a4ea386c4926d04
Viktorkvick <viktor.kvick@chasacademy.se>
2025-03-13 11:32:41 +0100

    Co-authored-by: Fredrik Storm <fredrik.storm@chasacademy.se> Co-authored-by: Marco-Lind-ChasAcademy <Marco-Lind-ChasAcademy@users.noreply.github.com>

--- src/main.cpp
+++ src/main.cpp
@@ -1,12 +1,27 @@
 #include <Arduino.h>
+#include <WiFiS3.h>
+#include <LiquidCrystal.h>
+#include "config.h"
+
+void ledControl(uint8_t ledPin, uint8_t state)
+{
+    analogWrite(ledPin, state);
+}
+
+void processLogic()
+{
+}
 
 void setup()
 {
     Serial.begin(9600);
+
+    pinMode(LED_PIN, OUTPUT);
 }
 
 void loop()
 {
-    Serial.println("Hello, World!");
+    processLogic();
+
     delay(1000);
 }
