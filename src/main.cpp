4dd10ee4e20bdd407b91ae68d400eeab8895127d
Wilmer-Chas <wilmer.forsman@chasacademy.se>
2025-03-14 10:27:38 +0100

    first version, adding a simplified version of reading gas/smoke values

--- src/main.cpp
+++ src/main.cpp
@@ -1,80 +1,25 @@
 #include <Arduino.h>
-#include <WiFiS3.h>
-#include <LiquidCrystal.h>
-#include <Wire.h>
-#include "config.h"
 
-LiquidCrystal lcd(LCD_RS, LCD_EN, LCD_D4, LCD_D5, LCD_D6, LCD_D7);
 
-void ledControl(uint8_t ledPin, uint8_t state)
-{
-    analogWrite(ledPin, state);
+int gasPin = A0; 
+int b2Pin = 0; 
+int gasLevel = 0;  
+
+
+
+void setup() {
+  Serial.begin(9600);
 }
 
-void lcdControl(LiquidCrystal lcd, uint8_t col, uint8_t row, String message)
-{
-    lcd.setCursor(col, row);
-    lcd.print(message);
+void loop() {
+  readGasValue();
 }
 
-void buzzerControl(uint8_t buzzerPin, uint8_t state)
+void readGasValue()
 {
-    analogWrite(buzzerPin, state);
-}
-
-void processLogic()
-{
-    lcdControl(lcd, 0, 0, "Example status");
-}
-
-void gasAlarm()
-{
-    float smokeLevel = analogRead(smokePin); // Read gas sensor value
-    Serial.print("Smoke Level: ");
-    Serial.println(smokeLevel); // Values for debugging
-
-    if (smokeLevel > smokeThreshold)
-    {
-        Serial.println(" Warning! Smoke detected!");
-        digitalWrite(buzzerPin, LOW);
-    }
-    else
-    {
-        Serial.println(" No Smoke Detected.");
-        digitalWrite(buzzerPin, HIGH);
-    }
-}
-
-void gasLight()
-{
-    float smokeLevel = analogRead(smokePin);
-
-    if (smokeLevel > smokeThreshold)
-    {
-        digitalWrite(ledPin, LOW);
-    }
-    else
-    {
-        digitalWrite(ledPin, HIGH);
-    }
-
-    delay(1000);
-}
-
-
-void setup()
-{
-    Serial.begin(9600);
-
-    lcd.begin(16, 2);
-
-    pinMode(LED_PIN, OUTPUT);
-    pinMode(BUZZER_PIN, OUTPUT);
-}
-
-void loop()
-{
-    processLogic();
-
-    delay(1000);
+  gasLevel = analogRead(gasPin); 
+  Serial.print("Gas Level: ");
+  Serial.println(gasLevel);  
+  
+  delay(1000);  
 }
