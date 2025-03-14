265fc5a1a6afeba638f13e3146c157504348ea86
Fredrik Storm <fredrik@fldc.se>
2025-03-14 14:50:21 +0100

    Only enable wifi when wifi_settings and set correct LCD pins.

--- src/config.h
+++ src/config.h
@@ -1,20 +1,17 @@
 #ifndef CONFIG_H
 #define CONFIG_H
 
-#define WIFI_ENABLED 0
-#define NTP_SERVER "pool.ntp.org"
-
 // pin configuration
 #define LED_PIN 3
 #define SMOKE_SENSOR_PIN 4
 #define BUZZER_PIN 5
 
-#define LCD_RS 7
-#define LCD_EN 8
-#define LCD_D4 9
+#define LCD_RS 13
+#define LCD_EN 12
+#define LCD_D4 11
 #define LCD_D5 10
-#define LCD_D6 11
-#define LCD_D7 12
+#define LCD_D6 9
+#define LCD_D7 8
 
 #define TURN_ON_HOUR 8
 #define TURN_OFF_HOUR 9
---
+++ src/wifi_settings.h
@@ -0,0 +1,18 @@
+#ifndef CREDENTIALS_H
+#define CREDENTIALS_H
+
+#define WIFI_ENABLED
+
+#define NTP_SERVER "pool.ntp.org"
+
+// WiFi credentials for the project
+#define WIFI_SSID "wifiofdoom"
+#define WIFI_PASSWORD "30229520"
+
+// MQTT broker credentials
+#define MQTT_SERVER "192.168.1.1"
+#define MQTT_PORT 1883
+#define MQTT_USER "chas"
+#define MQTT_PASSWORD "chas1234"
+
+#endif // CREDENTIALS_H
