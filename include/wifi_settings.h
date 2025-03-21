47ba5630db5483f738e32e5877c6f1d33187bea6
Fredrik Storm <fredrik@fldc.se>
2025-03-21 14:22:16 +0100

    Update wifi_settings.h

--- include/wifi_settings.h
+++ include/wifi_settings.h
@@ -5,14 +5,14 @@
 
 #define NTP_SERVER "pool.ntp.org"
 
-// WiFi credentials for the project
-#define WIFI_SSID "wifiofdoom"
-#define WIFI_PASSWORD "30229520"
+// WiFi credentials
+#define WIFI_SSID "SSID"
+#define WIFI_PASSWORD "PASSWORD"
 
 // MQTT broker credentials
 #define MQTT_SERVER "192.168.1.1"
 #define MQTT_PORT 1883
-#define MQTT_USER "chas"
-#define MQTT_PASSWORD "chas1234"
+#define MQTT_USER ""
+#define MQTT_PASSWORD ""
 
-#endif // CREDENTIALS_H
+#endif
